11a74f483ef17ff2d18c17a8c592fe24848ca9be
Feyistu Endale <fayistuendale@gmail.com>
2025-07-03 11:26:04 -0700

    Update If-Else.py

--- If-Else.py
+++ If-Else.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
 if n%2 !=0:
     print("Weird")
 else:
-    if 2<n<5:
+    if 2<=n<=5:
         print("Not Weird")
-    elif 6<n<20:
+    elif 6<=n<=20:
         print("Weird")
     else:
         print("Not Weird")
