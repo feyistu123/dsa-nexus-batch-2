1472ef7ce2eaf2d3298cb160f7ccbacb0f75e876
Feyistu Endale <fayistuendale@gmail.com>
2025-08-23 07:15:37 -0700

    Create partition-labels.py

---
+++ partition-labels.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_occur = {}
+        for i, char in enumerate(s):
+            last_occur[char] = i
+
+        result = []
+        start = 0
+        end = 0
+        for i , char in enumerate(s):
+            end = max(end, last_occur[char])
+            if end == i:
+                result.append(end - start + 1)
+                start = i + 1
+        return result
