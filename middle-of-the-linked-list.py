95b755538e0039350c1bf62e9d30cd24d0339884
Feyistu Endale <fayistuendale@gmail.com>
2025-08-24 13:52:38 -0700

    Create middle-of-the-linked-list.py

---
+++ middle-of-the-linked-list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        aheadptr = head
+        behindptr = head
+        while aheadptr and aheadptr.next:
+            aheadptr = aheadptr.next.next
+            behindptr = behindptr.next
+        return behindptr
+        
